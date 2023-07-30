527f7e2db0500ed5a8899d105e5200936bba360e
alexanderkenrick <alexanderkenrick3@gmail.com>
2023-07-31 00:47:34 +0700

    Update Map

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,251 +2,28 @@
 
 @section('css')
     <link href="https://unpkg.com/nes.css@2.3.0/css/nes.min.css" rel="stylesheet" />
+    <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
     <style type="text/css">
         /* 40128b */
 
 
-        body {
-            background: #57c1e5;
-        }
-
-        .rotate-phone-text {
-            font-family: 'Broken Console', sans-serif;
-        }
-
-        .map-container {
-            text-align: center;
-            position: relative;
-            width: 700px;
-            height: 417px;
-            background-color: #57c1e5;
-            z-index: -1;
-        }
-
-        .map_image {
-            width: 700px;
-            height: 417px;
-        }
-
-        .point {
-            position: absolute;
-        }
-
-        #ta-bw {
-            width: 83px;
-            height: auto;
-            bottom: 113px;
-            left: 289.3px;
-        }
-
-        #tb-bw {
-            width: 100px;
-            height: auto;
-            bottom: 143px;
-            right: 236px;
-        }
-
-        #tc-bw {
-            width: 107px;
-            height: auto;
-            top: 146px;
-            right: 139px;
-            z-index: 1;
-        }
-
-        #td-bw {
-            width: 77px;
-            height: auto;
-            top: 94px;
-            right: 146px;
-            /* z-index: 1; */
-        }
-
-        #te-bw {
-            width: 129px;
-            height: auto;
-            top: 92px;
-            right: 233px;
-            z-index: 1;
-        }
-
-        #tf-bw {
-            width: 142px;
-            height: auto;
-            bottom: 165px;
-            left: 205px;
-        }
-
-        #tg-bw {
-            width: 108px;
-            height: auto;
-            top: 15px;
-            right: 228px;
-        }
-
-        .blocker {
-            display: none;
-        }
-
-        .broken-console {
-            font-family: 'Broken Console', sans-serif !important;
-        }
-
-        .dialog-menu{
-            display: flex;
-            flex-direction: row;
-            justify-content: space-between;
-        }
-
-
-
-        @media screen and (orientation: portrait) {
-            .map-section {
-                display: none;
-            }
-
-            .blocker {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                width: 100vw;
-                height: 100vh;
-                position: fixed;
-                top: 0;
-                left: 0;
-                background-color: #000;
-                z-index: 1000;
-                color: #ffffff;
-            }
-        }
-
-        .nes-input {
-            width: 400px;
-            border-image-repeat: stretch !important;
-            background-clip: unset !important;
-            border-width: 6px !important;
-            height: 40px;
-
-        }
-
-        .nes-dialog.is-rounded,
-        .nes-btn,
-        .nes-textarea {
-            border-image-repeat: stretch !important;
-        }
-
-        .nes-btn {
-            font-family: 'Broken Console', sans-serif !important;
-        }
-
-        #dialog-rounded {
-            height: 400px;
-            overflow-y: auto;
-            min-width: 400px;
-            max-width: 700px;
-            overflow-y: auto;
-        }
-
-        #answer {
-            height: 80px;
-        }
-
-        #awan-1 {
-            position: absolute;
-            width: 200px;
-            height: auto;
-            top: 20px;
-            left: 60px;
-            z-index: -1;
-            animation: awan1 2s infinite ease-in-out;
-        }
-
-        #awan-3{
-            position: absolute;
-            width: 180px;
-            height: auto;
-            top: 50px;
-            right: 60px;
-            z-index: -1;
-            animation: awan1 2s infinite ease-in-out;
-        }
-
-        #awan-5{
-            position: absolute;
-            width: 150px;
-            height: auto;
-            top: 50px;
-            left: 40px;
-            z-index: 2;
-            animation: awan1 2s infinite ease-in-out;
-        }
-
-        #bird-1 {
-            position: absolute;
-            width: 50px;
-            top: 20px;
-            left: -20px;
-            transform: translateX(-30px);
-            animation: angrybird 2s infinite linear;
-            z-index: -2;
-        }
-
-        .hidden{
-            display: none;
-        }
-
-        @keyframes awan1{
-            0% {
-                transform: translate(0, 0px);
-            }
-
-            50% {
-                transform: translate(0, 5px);
-            }
-
-            100% {
-                transform: translateY(0, -0px);
-            }
-        }
-
-        @keyframes angrybird {
-
-            0% {
-                transform: translate(0, 0px);
-            }
-
-            25%{
-                transform: translate(30px, 15px);
-            }
-
-            50% {
-                transform: translate(60px, 0px);
-            }
-
-            75%{
-                transform: translate(90px, 15px);
-            }
-
-            100% {
-                transform: translate(120px, -0px);
-            }
-        }
+       
     </style>
 @endsection
 
 @section('content')
-    <div class="blocker">
+    {{-- <div class="blocker">
         <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
         <lottie-player src="https://lottie.host/f0c83d85-3b47-4b77-990c-3245f810b85e/dguz1WhP5f.json" background="#000000"
             speed="1" style="width: 250px; height: 250px" direction="1" mode="normal" loop autoplay></lottie-player>
         <h2 class="rotate-phone-text">Please Rotate Your Phone</h2>
-    </div>
+    </div> --}}
 
     <section class="map-section">
-        <img src="{{ asset('../images/assets_gedung/awan-1.png') }}" id="awan-1" alt="">
+        {{-- <img src="{{ asset('../images/assets_gedung/awan-1.png') }}" id="awan-1" alt="">
         <img src="{{ asset('../images/assets_gedung/awan-3.png') }}" id="awan-3" alt="">
         <img src="{{ asset('../images/assets_gedung/awan-5.png') }}" id="awan-5" alt="">
-        <img src="{{ asset('../images/assets_gedung/bird.png') }}" id="bird-1" alt="">
+        <img src="{{ asset('../images/assets_gedung/bird.png') }}" id="bird-1" alt=""> --}}
         <h1 class="title" style="z-index:5">Engineering Tour</h1>
         <div class="container-fluid my-0 my-4">
             <div class="row">
