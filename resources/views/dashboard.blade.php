224b216f109e8bfed6542e748063390c6ce4f77e
alexanderkenrick <alexanderkenrick3@gmail.com>
2023-07-31 00:50:29 +0700

    hehe

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,7 +24,7 @@
         <img src="{{ asset('../images/assets_gedung/awan-3.png') }}" id="awan-3" alt="">
         <img src="{{ asset('../images/assets_gedung/awan-5.png') }}" id="awan-5" alt="">
         <img src="{{ asset('../images/assets_gedung/bird.png') }}" id="bird-1" alt=""> --}}
-        <h1 class="title" style="z-index:5">Engineering Tour</h1>
+        <h1 class="title mt-4" style="z-index:5">Engineering Tour</h1>
         <div class="container-fluid my-0 my-4">
             <div class="row my-5">
                 <div class="col  d-flex justify-content-center">
