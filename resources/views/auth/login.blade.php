d4d196756ec439525a217a514a6b6e743345cd6e
alexanderkenrick <alexanderkenrick3@gmail.com>
2023-08-13 00:15:03 +0700

    add checkpoint

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,66 +1,78 @@
 @extends('layouts.app')
 
 @section('content')
-    {{-- Navbar --}}
-    <header class="navbar navbar-expand-lg sticky-top nav-mob bg-transparent">
-        <nav class="container flex-wrap flex-lg-nowrap">
-            <a class="navbar-brand fw-semibold fs-3 text-mob">MOB FT 2023</a>
-        </nav>
-    </header>
-    {{-- End Navbar --}}
-    <div class="container cotainer-size">
-        <div class="card p-4">
-            <div class="row g-0 justify-content-center">
-                <div class="col-md-4 logo">
-                    <img src="{{ asset('images/logo.png') }}" class="img-fluid">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}" class="form-floating">
-                            @csrf
-                            <h2 class="text-mob">Masuk</h2>
-                            <div class="form-floating mb-3">
-                                <input id="username" type="username"
-                                    class="form-control @error('username') is-invalid @enderror form-control-epass"
-                                    name="username" value="{{ old('username') }}" required autocomplete="off" autofocus
-                                    placeholder="username">
-                                <label for="username">{{ __('Username') }}</label>
+<link href="{{ asset('css/login.css') }}" rel="stylesheet">
+<link href="https://unpkg.com/nes.css@2.3.0/css/nes.min.css" rel="stylesheet" />
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <div class="title-container">
+                        <img src="{{ asset('images/logo.png') }}" alt="" class="image">
+                        <h2 class="title mt-3">LOGIN</h2>
+                    </div>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
+                        </div>
 
-                            <div class="form-floating">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror form-control-epass"
-                                    name="password" required autocomplete="off" placeholder="kata sandi">
-                                <label for="password">{{ __('Kata Sandi') }}</label>
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
+                        </div>
 
-                            <div class="form-group row mb-3 mt-3">
-                                <div class="col-md-12">
-                                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                        <button type="submit" class="btn btn-primary">
-                                            {{ __('Login') }}
-                                        </button>
-                                    </div>
+                        {{-- <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
                                 </div>
                             </div>
+                        </div> --}}
 
-                        </form>
-                    </div>
+                        <div class="row mb-0 mt-4 d-flex justify-content-center">
+                            <div class="col-md-8 ">
+                                <button type="submit" class="nes-btn is-primary w-100" style='font-family: "04b", sans-serif;'>
+                                    {{ __('Login') }}
+                                </button>
+
+                                {{-- @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif --}}
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
-
-
-
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -58,7 +58,7 @@
                             <img src="{{ asset('../images/assets_gedung/bw/TG_bw.png') }}" alt="" class="point"
                                 id="tg-bw">
                         @endif
-
+                        <img src="{{ asset('../images/checkpoint.png') }}" class="check-tf" id="checkpoint" alt="">
                     </div>
                 </div>
             </div>
